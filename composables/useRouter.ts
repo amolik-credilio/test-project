9c3be898720f99bc98f72e46b6fff76b30a55fbf
amolik-credilio <amolik.paul@credilio.in>
2023-12-06 17:38:41 +0530

    Added: useRouter composable

--- composables/useRouter.ts
+++ composables/useRouter.ts
@@ -1,6 +1,6 @@
 import { getCurrentInstance } from 'vue'
 
-export default function useRoute() {
+export default function useRouter() {
   const vm = getCurrentInstance()
   if (!vm) throw new Error('must be called in setup')
 
